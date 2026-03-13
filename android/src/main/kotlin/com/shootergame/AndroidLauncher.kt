408a61a9a2445656dc5d5e0934e17a9fca472e75
redel1290 <denismakusko@gmail.com>
2026-03-13 10:52:39 +0200

    Debug: show crash error as Toast

--- android/src/main/kotlin/com/shootergame/AndroidLauncher.kt
+++ android/src/main/kotlin/com/shootergame/AndroidLauncher.kt
@@ -1,16 +1,22 @@
 package com.shootergame
 
 import android.os.Bundle
+import android.widget.Toast
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 
 class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val config = AndroidApplicationConfiguration().apply {
-            useImmersiveMode = true
-            useWakelock = true
+        try {
+            val config = AndroidApplicationConfiguration().apply {
+                useImmersiveMode = true
+                useWakelock = true
+            }
+            initialize(ShooterGame(), config)
+        } catch (e: Exception) {
+            Toast.makeText(this, "Помилка: ${e.message} / ${e.cause}", Toast.LENGTH_LONG).show()
+            e.printStackTrace()
         }
-        initialize(ShooterGame(), config)
     }
 }
